0ecb4bf6f6918bccc7edbb2a2ec8241f034c8fdd
Heewa Barfchin <heewa.b@gmail.com>
2020-12-18 21:18:26 -0500

    Stop naming combos

--- keyboards/handwired/kaj_kuchik/keymaps/default/combos.h
+++ keyboards/handwired/kaj_kuchik/keymaps/default/combos.h
@@ -1,29 +1,5 @@
 #include "kb.h"
 
-enum combos {
-    // Punctuation
-    KL_QUOTE,
-    MC_DOT,
-    XC_BSLASH,
-    QW_TICK,
-    OP_SCLN,
-    NM_SLSH,
-
-    // Brackets
-    DF_LPRN,
-    HJ_RPRN,
-    ER_LBRC,
-    UI_RBRC,
-    RT_LT,
-    YU_GT,
-    FG_LCBR,
-    SCH_RCBR,
-
-    // +/-
-    CV_MINUS,
-    BN_EQL,
-};
-
 const uint16_t PROGMEM kl_combo[] = {KC_K, KC_L, COMBO_END};
 const uint16_t PROGMEM mc_combo[] = {KC_M, KC_COMM, COMBO_END};
 const uint16_t PROGMEM xc_combo[] = {KC_X, KC_C, COMBO_END};
@@ -32,6 +8,7 @@ const uint16_t PROGMEM as_combo[] = {KC_A, KC_S, COMBO_END};
 const uint16_t PROGMEM op_combo[] = {KC_O, KC_P, COMBO_END};
 const uint16_t PROGMEM df_combo[] = {KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM hj_combo[] = {KC_H, KC_J, COMBO_END};
+const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM er_combo[] = {KC_E, KC_R, COMBO_END};
 const uint16_t PROGMEM ui_combo[] = {KC_U, KC_I, COMBO_END};
 const uint16_t PROGMEM rt_combo[] = {KC_R, KC_T, COMBO_END};
@@ -43,20 +20,20 @@ const uint16_t PROGMEM bn_combo[] = {KC_B, KC_N, COMBO_END};
 const uint16_t PROGMEM nm_combo[] = {KC_N, KC_M, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-    [KL_QUOTE] = COMBO(kl_combo, KC_QUOT),
-    [MC_DOT] = COMBO(mc_combo, KC_DOT),
-    [XC_BSLASH] = COMBO(xc_combo, KC_BSLASH),
-    [QW_TICK] = COMBO(qw_combo, KC_GRV),
-    [OP_SCLN] = COMBO(op_combo, KC_SCLN),
-    [NM_SLSH] = COMBO(nm_combo, KC_SLSH),
-    [DF_LPRN] = COMBO(df_combo, KC_LPRN),
-    [HJ_RPRN] = COMBO(hj_combo, KC_RPRN),
-    [ER_LBRC] = COMBO(er_combo, KC_LBRC),
-    [UI_RBRC] = COMBO(ui_combo, KC_RBRC),
-    [RT_LT] = COMBO(rt_combo, KC_LT),
-    [YU_GT] = COMBO(yu_combo, KC_GT),
-    [FG_LCBR] = COMBO(fg_combo, KC_LCBR),
-    [SCH_RCBR] = COMBO(sch_combo, KC_RCBR),
-    [CV_MINUS] = COMBO(cv_combo, KC_MINUS),
-    [BN_EQL] = COMBO(bn_combo, KC_EQL),
+    COMBO(jk_combo, KC_QUOT),
+    COMBO(mc_combo, KC_EQL),
+    COMBO(xc_combo, KC_BSLASH),
+    COMBO(qw_combo, KC_GRV),
+    COMBO(op_combo, KC_SCLN),
+    COMBO(nm_combo, KC_SLSH),
+    COMBO(df_combo, KC_LPRN),
+    COMBO(hj_combo, KC_RPRN),
+    COMBO(er_combo, KC_LBRC),
+    COMBO(ui_combo, KC_RBRC),
+    COMBO(rt_combo, KC_LT),
+    COMBO(yu_combo, KC_GT),
+    COMBO(fg_combo, KC_LCBR),
+    COMBO(sch_combo, KC_RCBR),
+    COMBO(cv_combo, KC_MINUS),
+    COMBO(bn_combo, KC_DOT),
 };
