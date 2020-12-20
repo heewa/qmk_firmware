467d4e63f9e966b96554a1e21e618de4fb934dc4
Heewa Barfchin <heewa.b@gmail.com>
2020-12-20 16:57:13 -0500

    Improve keymap layout and add reset key to ErgoFlirt

--- keyboards/handwired/ergoflirt/stagger/rules.mk
+++ keyboards/handwired/ergoflirt/stagger/rules.mk
@@ -46,8 +46,8 @@ OPT_DEFS += -DBOOTLOADER_SIZE=4096
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE ?= yes	# Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE ?= yes	# Mouse keys(+4700)
+BOOTMAGIC_ENABLE ?= lite	# Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE ?= no	# Mouse keys(+4700)
 EXTRAKEY_ENABLE ?= yes	# Audio control and System control(+450)
 CONSOLE_ENABLE ?= no	# Console for debug(+400)
 COMMAND_ENABLE ?= no    # Commands for debug and configuration
@@ -56,3 +56,4 @@ NKRO_ENABLE ?= yes		# USB Nkey Rollover - if this doesn't work, see here: https:
 BACKLIGHT_ENABLE ?= no  # Enable keyboard backlight functionality
 AUDIO_ENABLE ?= no
 RGBLIGHT_ENABLE ?= no
+TAP_DANCE_ENABLE ?= yes
