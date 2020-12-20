dec8d691239456a7d0d5f7391fa0323f879a7d80
Heewa Barfchin <heewa.b@gmail.com>
2020-12-20 14:43:11 -0500

    Move around nums & symbols

--- keyboards/handwired/kaj_kuchik/keymaps/default/keymap.c
+++ keyboards/handwired/kaj_kuchik/keymaps/default/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                KC_B,
                 KC_N,
                  KC_M,
-                  KC_COMM,
+                  LT(3, KC_COMM),
         KC_BSPC,
           KC_RGUI,
           CTL_T(KC_ESC),
@@ -62,22 +62,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Layer 1
 	KEYMAP(
-		KC_1,
-         KC_2,
-          KC_3,
-           KC_4,
-            KC_5,
-                KC_6,
-                 KC_7,
-                  KC_8,
-                   KC_9,
-                    KC_0,
-		____,
+		LSFT(KC_1),
+         LSFT(KC_2),
+          LSFT(KC_3),
+           LSFT(KC_4),
+            LSFT(KC_5),
+                LSFT(KC_6),
+                 LSFT(KC_7),
+                  LSFT(KC_8),
+                   LSFT(KC_9),
+                    LSFT(KC_0),
+		LSFT(KC_SLSH),
          ____,
           ____,
            ____,
             ____,
-                ____,
+                KC_SCLN,
                  KC_LEFT,
                   KC_DOWN,
                    KC_UP,
@@ -135,6 +135,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_TAB,
           LALT(KC_ENT)),
 
+    // Layer 3
+	KEYMAP(
+		____,
+         ____,
+          ____,
+           ____,
+            ____,
+                ____,
+                 KC_7,
+                  KC_8,
+                   KC_9,
+                    ____,
+		____,
+         ____,
+          ____,
+           ____,
+            ____,
+                KC_0,
+                 KC_4,
+                  KC_5,
+                   KC_6,
+                    KC_0,
+		____,
+         ____,
+          ____,
+           ____,
+               KC_1,
+                KC_2,
+                 KC_3,
+                  ____,
+        ____,
+          ____,
+          ____,
+        ____,
+          ____,
+          ____),
+
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
@@ -153,6 +190,27 @@ void matrix_scan_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    /*
+    // handle reverse shifted keys
+    uint8_t mod_state = get_mods();
+    switch (keycode) {
+        case KC_SCLN:
+            if (record->event.pressed) {
+                if (mod_state & MOD_MASK_SHIFT) {
+                    del_mods(MOD_MASK_SHIFT);
+                } else {
+                    add_mods(MOD_MASK_SHIFT);
+                }
+                register_code(keycode);
+                set_mods(mod_state);
+
+                return false;
+            }
+
+            break;
+    }
+    */
+
 	return true;
 }
 
