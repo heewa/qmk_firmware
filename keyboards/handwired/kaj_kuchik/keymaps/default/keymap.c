38eeac1086af3996feba1d4659144c96d9f79bd6
Heewa Barfchin <heewa.b@gmail.com>
2020-12-21 12:52:30 -0500

    Shift slash so excited punctuation is easier

--- keyboards/handwired/kaj_kuchik/keymaps/default/keymap.c
+++ keyboards/handwired/kaj_kuchik/keymaps/default/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                   KC_8,
                    KC_9,
                     KC_0,
-		KC_SLSH,
+		LSFT(KC_SLSH),
          ____,
           ____,
            ____,
