675c74e4bc49caa934dfa71b4f02d29895ef1503
Heewa Barfchin <heewa.b@gmail.com>
2020-12-24 14:22:59 -0500

    Enable and map mouse keys

--- keyboards/handwired/kaj34/rev1/config.h
+++ keyboards/handwired/kaj34/rev1/config.h
@@ -8,3 +8,7 @@
 #define MATRIX_ROW_PINS { B1, C6, B2, B5, E6, B4 }
 #define MATRIX_COL_PINS { F6, F7, B3, D7, D4, B6 }
 #define UNUSED_PINS
+
+#define MOUSEKEY_DELAY 300
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_MAX_SPEED 3
