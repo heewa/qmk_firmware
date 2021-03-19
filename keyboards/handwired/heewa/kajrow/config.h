5f3c5877cb7e2544968a3d47bb9ea0b580595ed3
Heewa Barfchin <heewa.b@gmail.com>
2021-03-19 19:29:22 -0400

    Add a new keyboard: KajRow

---
+++ keyboards/handwired/heewa/kajrow/config.h
@@ -0,0 +1,62 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "config_common.h"
+
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xBAAF
+#define PRODUCT_ID      0x4202
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    HeewaBarfchin
+#define PRODUCT         KajRow
+#define DESCRIPTION     Keyboard
+
+/* key matrix size */
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 8
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { D1, D4, D7, B4, F4, F6, B1, B2 }
+#define MATRIX_COL_PINS { D0, C6, E6, B5, B6, B3, F7, F5 }
+#define UNUSED_PINS { }
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* number of backlight levels */
+
+#ifdef BACKLIGHT_PIN
+#define BACKLIGHT_LEVELS 3
+#endif
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCING_DELAY 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* key combination for command */
+#define IS_COMMAND() ( \
+    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
+)
+
+/* prevent stuck modifiers */
+#define PREVENT_STUCK_MODIFIERS
+
+
+#ifdef RGB_DI_PIN
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 0
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+#endif
+
+#endif
---
+++ keyboards/handwired/heewa/kajrow/keymaps/default/keymap.c
@@ -0,0 +1,105 @@
+#include "kb.h"
+
+#define ____ KC_TRNS
+
+enum {
+    TD_RESET,
+};
+
+void handle_reset_tap(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count >= 4) {
+        reset_keyboard();
+        reset_tap_dance(state);
+    } else {
+        tap_code(KC_F1);
+    }
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_RESET] = ACTION_TAP_DANCE_FN(handle_reset_tap),
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	KEYMAP(
+		KC_1, KC_2, KC_3, KC_4, KC_5,
+		KC_Q, KC_W, KC_E, KC_R, KC_T,
+		KC_A, KC_S,  KC_D, KC_F, KC_G,
+		KC_GRV, KC_Z, KC_X, KC_C, KC_V, KC_B,
+
+        KC_BSPC, KC_RGUI, CTL_T(KC_ESC),
+        LT(1, KC_TAB), ALT_T(KC_ENT), SFT_T(KC_SPC),
+
+		KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS,
+		KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,
+		KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_UP,
+		KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LEFT, KC_DOWN, KC_RIGHT),
+
+	KEYMAP(
+		TD(TD_RESET), KC_F2, KC_F3, KC_F4, KC_F5,
+        ____, ____, ____, ____, ____,
+        KC__VOLDOWN, KC__MUTE, KC__VOLUP, ____, ____,
+        ____, KC_MPRV, KC_MPLY, KC_MNXT, ____, ____,
+
+        KC_DEL, ____, ____,
+        ____, ____, KC_INS,
+
+        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, ____,
+        ____, ____, ____, ____, ____, ____, ____,
+        KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, ____, ____, KC_PGUP,
+        ____, ____, ____, ____, ____, KC_HOME, KC_PGDN, KC_END),
+
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+	//keyevent_t event = record->event;
+
+	switch (id) {
+
+	}
+	return MACRO_NONE;
+}
+
+void matrix_init_user(void) {
+}
+
+void matrix_scan_user(void) {
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	return true;
+}
+
+void led_set_user(uint8_t usb_led) {
+
+	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
+
+	} else {
+
+	}
+
+	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
+
+	} else {
+
+	}
+
+	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
+
+	} else {
+
+	}
+
+	if (usb_led & (1 << USB_LED_COMPOSE)) {
+
+	} else {
+
+	}
+
+	if (usb_led & (1 << USB_LED_KANA)) {
+
+	} else {
+
+	}
+
+}
